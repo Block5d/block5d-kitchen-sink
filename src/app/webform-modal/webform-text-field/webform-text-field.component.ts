c99a542e804154cb60b08a3200eb85105a7561f0
Mr-Mooo <hjchoujiacheng@126.com>
2017-12-23 02:02:33 +0800

    WebForm Fields

--- src/app/webform-modal/webform-text-field/webform-text-field.component.ts
+++ src/app/webform-modal/webform-text-field/webform-text-field.component.ts
@@ -9,7 +9,17 @@ import { WebformTextField } from '../../shared/webform-modal';
 export class WebformTextFieldComponent implements OnInit {
 
   textFieldModel = new WebformTextField('', '', '');
-
+  models=[{label:"Name",placeholder:"Admin",description:"Your Name"},
+  {label:"Address",placeholder:"Address",description:"Address"}]
+  change(nzValue){
+   //console.log(nzValue)
+   for(let i=0;i<this.models.length;i++){
+    if(this.models[i].label==nzValue){
+      this.textFieldModel=this.models[i];
+      //console.log(this.textFieldModel)
+    }
+   }
+  }
   constructor() { }
 
   ngOnInit() {
